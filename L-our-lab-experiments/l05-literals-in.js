262b85749d611711de3b552a4a02334815926c9b
Jabi <michal.jabi@gmail.com>
2024-06-11 13:16:18 +0200

    explain (l05): literals concept in JS.

---
+++ L-our-lab-experiments/l05-literals-in.js
@@ -0,0 +1,17 @@
+// 
+const sth = new RegExp();
+const sth2 = /(?:)/;
+
+console.log(sth)
+
+console.log(sth2)
+
+
+console.log('Array literal', []);
+// instead of new Array()
+
+console.log('Object literal', {});
+// instead of new Object()
+
+console.log('RegExp literal', /()/);
+// instead of new RegExp()
