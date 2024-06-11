721c2b6b01c3ab60fde628d7366fc487677fcf84
Jabi <michal.jabi@gmail.com>
2024-06-11 15:12:58 +0200

    train(e02): make it work.

--- D-control-statements-and-error-handlng/e02-train-loops.js
+++ D-control-statements-and-error-handlng/e02-train-loops.js
@@ -4,9 +4,21 @@
 // 2
 // 3
 // 4
-
+for (let x = 1; x <= 10; x++) {
+    console.log(x);
+}
 
 // #2 Task
 // Show a sequence of numbers 1 to 10 next to each other on the console:
 // 1 2 3 4 5 6 7 8 9 10
 
+let numbersStr = '';
+const numbers = [];
+
+for (let x = 1; x <= 10; x++) {
+    numbersStr = numbersStr + ' ' + x;
+    numbers.push(x);
+}
+
+console.log(numbersStr.trim())
+console.log(numbers.join(' '))
