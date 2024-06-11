fa043ef47b38fe4780a11337bf312356a754e298
Jabi <michal.jabi@gmail.com>
2024-06-11 10:20:46 +0200

    task(c02): solved.

--- C-complex-types-using-objects-and-arrays/c02-objects-more-complex.js
+++ C-complex-types-using-objects-and-arrays/c02-objects-more-complex.js
@@ -13,13 +13,19 @@ const user = {
 		shireCounty: 'Lincolnshire',
 		house: {
 			no: 23
-		}
+		},
+		district: '' // good practice to have whole shape since begining
 	},
 	isRegistered: true
 };
 
 // #1 Task:
 // Print on console - the "no" of the house form user:
+console.log(user.address.house.no)
 
 // #2 Task:
 // Add "district" "East Lindsey" to the user address
+user.address.district = 'East Lindsey'
+
+console.log(user.address);
+console.log(user.address.district);
