1608402fa08c2d77911122cb758fbdd6000aa4c0
Jabi <michal.jabi@gmail.com>
2024-06-11 14:41:45 +0200

    explain (l06): explain use-case of having anything inside condition logic.

--- L-our-lab-experiments/l06-reg-exp-example.js
+++ L-our-lab-experiments/l06-reg-exp-example.js
@@ -6,3 +6,20 @@ const matches = phrase.match(/a\wc\d+/g);
 
 
 console.log(matches);
+
+const phrase2 = 'some text with and again'
+
+const matches2 = phrase2.match(/a\wc\d+/g);
+
+console.log(matches2)
+
+
+// Benefits of not need to put logical expressions here
+// instead we put here null or Array object.
+if (matches2) {
+    console.log('we got something', matches2)
+}
+
+if (matches) {
+    console.log('we got something', matches)
+}
