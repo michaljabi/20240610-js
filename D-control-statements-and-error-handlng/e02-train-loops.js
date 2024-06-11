b0f1ec3dfbee96b9d930a87601093e64528a854f
Jabi <michal.jabi@gmail.com>
2024-06-11 15:15:06 +0200

    train(e02): make it fast (pahse 3 optimise);

--- D-control-statements-and-error-handlng/e02-train-loops.js
+++ D-control-statements-and-error-handlng/e02-train-loops.js
@@ -4,22 +4,26 @@
 // 2
 // 3
 // 4
+let numbersStr = '';
+const numbers = [];
+
 for (let x = 1; x <= 10; x++) {
     console.log(x);
+    numbersStr += ' ' + x;
+    numbers.push(x);
 }
 
 // #2 Task
 // Show a sequence of numbers 1 to 10 next to each other on the console:
 // 1 2 3 4 5 6 7 8 9 10
 
-let numbersStr = '';
-const numbers = [];
 
-for (let x = 1; x <= 10; x++) {
-    // numbersStr = numbersStr + ' ' + x;
-    numbersStr += ' ' + x;
-    numbers.push(x);
-}
+
+// for (let x = 1; x <= 10; x++) {
+//     // numbersStr = numbersStr + ' ' + x;
+//     numbersStr += ' ' + x;
+//     numbers.push(x);
+// }
 
 console.log(numbersStr.trim())
 console.log(numbers.join(' '))
