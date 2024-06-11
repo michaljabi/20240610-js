2d5f37c1d201829be84c6902b019a6a696e6876b
Jabi <michal.jabi@gmail.com>
2024-06-11 10:58:04 +0200

    explain(c03): methods + objects spread ... - so called shallow copy.

--- L-our-lab-experiments/l03-return-keyword-problem.js
+++ L-our-lab-experiments/l03-return-keyword-problem.js
@@ -1,8 +1,13 @@
 
 function makeUser(name2 = 'Katie') { // factory function
-    return {
+    return
+    {
         name: name2
     };
+    // We did not start object with same line as return,
+    // that is why object is in "unreachable code" zone!
+    // under the hood JS did that:
+    // return;
 }
 
 console.log(makeUser())
--- L-our-lab-experiments/l04-when-i-want-to-use-same-property-in-objcect.js
+++ L-our-lab-experiments/l04-when-i-want-to-use-same-property-in-objcect.js
@@ -15,4 +15,21 @@ console.log({
     id: 1,
     name: 'Michal',
     id: undefined
-})
+});
+
+
+// 
+// -----
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+// Mark ased about it :)
+
+function addNumbers(n1, n2, n3) {
+    return n1 + n2 + n3;
+}
+
+// [1, 2, 3];
+
+addNumbers.apply(null, [1, 2, 3]); //=
+
+addNumbers(...[1, 2, 3]); // addNumbers(1, 2, 3) 
+addNumbers(...[1, 2, 3]); //=
