df4367da1fbeb4480c412a259cd1dbcc912ae813
Jabi <michal.jabi@gmail.com>
2024-06-12 13:57:29 +0200

    refactor(f09): step2: another solution .forEach() metod on the array.

--- F-javascript-inside-html/f09-task-ems-modules/main.js
+++ F-javascript-inside-html/f09-task-ems-modules/main.js
@@ -5,3 +5,7 @@ const u1 = document.querySelector('[data-list]');
 for (const fruit of fruits) {
     u1.innerHTML += `<li>${fruit}</li>`
 }
+
+// fruits.forEach((fruit) => {
+//     u1.innerHTML += `<li>${fruit}</li>`
+// })
